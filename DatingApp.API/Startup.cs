63f5c680b07ea9ec36338f34ada3d9c16baa013c
Diocleziano Carletti <dcarletti@gruppocabel.eu>
2020-01-22 16:54:09 +0100

    Metodi Registrazione

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -29,6 +29,7 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddCors(); // --> abilitazione del CORS per far comunicare il client con l'API.
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
